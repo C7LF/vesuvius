1272a01731e4355bffa79cf5a759aaaa8dd0f1fd
C7LF <ccal72@yahoo.co.uk>
2020-08-19 22:50:26 +0100

    embeded message formatting

--- index.js
+++ index.js
@@ -13,16 +13,12 @@ const DATA = require('./services/freeGames');
 bot.login(TOKEN)
 
 bot.on('ready', async () => {
-  // Every Thursday at 18:00 UTC
-  // const job = new CronJob('0 18 * * 4', () => {
-  //bot.channels.get('572466530235973644').send(DATA.freeGameData)
-  // }, null, true, 'UTC')
 
   const TodayFormatted = new Date().toLocaleDateString()
 
   const EpicFreeGames = new EmbeddedMessage()
-    .setTitle(`Free Games - ${TodayFormatted}`)
-    .setAuthor('Epic', 'https://img.icons8.com/nolan/2x/epic-games.png', 'https://www.epicgames.com/store/en-US/free-games')
+    .setTitle(`${TodayFormatted}`)
+    .setAuthor('Epic Free Games', 'https://img.icons8.com/nolan/2x/epic-games.png', 'https://www.epicgames.com/store/en-US/free-games')
     .setColor('#0078F2')
 
   // The Games
@@ -30,11 +26,13 @@ bot.on('ready', async () => {
     EpicFreeGames.setDescription(response.map(item => item.title))
   )
 
-  const job = new CronJob('* * * * *', () => {
+  // Every Thursday at 18:00 UTC
+  const job = new CronJob('0 18 * * 4', () => {
     bot.channels.cache.get('572466530235973644').send(EpicFreeGames)
   }, null, true, 'UTC')
 
   job.start()
+
   console.info(`Logged in as ${bot.user.tag}!`);
 })
 

7f0780564507cc9af7b61be410deee072b0015c0
C7LF <ccal72@yahoo.co.uk>
2022-01-20 11:07:47 +0000

    Refactor commands into class

--- src/commands/w2g/w2g.ts
+++ src/commands/w2g/w2g.ts
@@ -1,26 +1,26 @@
-import { prefix } from "../../config/command-config";
-import { Command } from "../../models";
+import { Message } from "discord.js";
+import VesuviusCommand from "../../models/vesuvius-command.class";
 import { createRoom } from "../../services/create-room";
 
-const WatchTogether: Command = {
-  name: `${prefix}w2g`,
-  description: "Create a temporary watch together room",
-  async execute(msg, args) {
-    // Base Url
-    let mes = "https://w2g.tv/rooms/";
+const watchTogetherExecute = () => async (msg: Message, args?: string[]) => {
+  // Base Url
+  let mes = "https://w2g.tv/rooms/";
 
-    // Remove embedded message
-    msg.suppressEmbeds(true);
+  // Remove embedded message
+  msg.suppressEmbeds(true);
 
-    if (args) {
-      await createRoom(args[0])
-        .then((response) => (mes += response.streamkey))
-        .catch((err) => console.log("Error creating room: ", err));
-      msg.channel.send(mes);
-    } else {
-      msg.channel.send("Error executing command");
-    }
-  },
+  if (args) {
+    await createRoom(args[0])
+      .then((response) => (mes += response.streamkey))
+      .catch((err) => console.log("Error creating room: ", err));
+    msg.channel.send(mes);
+  } else {
+    msg.channel.send("Error executing command");
+  }
 };
 
-export = WatchTogether;
+export = new VesuviusCommand(
+  "w2g",
+  "Create a temporary watch together room",
+  watchTogetherExecute()
+);
--- src/commands/yt-audio/stop.ts
+++ src/commands/yt-audio/stop.ts
@@ -1,22 +1,18 @@
-import { prefix } from "../../config/command-config";
-import { Command } from "../../models";
+import VesuviusCommand from "../../models/vesuvius-command.class";
 
-const StopCommand: Command = {
-  name: `${prefix}stop`,
-  description: "Stop the songs in the queue",
-  execute(msg: any): void {
-    const serverQueue = msg.client.queue.get(msg.guild.id);
-
-    if (!serverQueue) return msg.channel.send("No song playing, fool!");
-
-    if (!msg.member.voice.channel)
-      return msg.channel.send(
-        "You have to be in a voice channel to stop the music!"
-      );
-
-    serverQueue.songs = [];
-    serverQueue.connection.dispatcher.end();
-  },
+const stopExecute = () => (msg: any) => {
+  const serverQueue = msg.client.queue.get(msg.guild.id);
+  if (!serverQueue) return msg.channel.send("No song playing, fool!");
+  if (!msg.member.voice.channel)
+    return msg.channel.send(
+      "You have to be in a voice channel to stop the music!"
+    );
+  serverQueue.songs = [];
+  serverQueue.connection.dispatcher.end();
 };
 
-export = StopCommand;
+export = new VesuviusCommand(
+  "stop",
+  "Stop the songs in the queue",
+  stopExecute()
+);

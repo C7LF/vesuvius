4158f2f75aaa5f7aa4a547f1444ef1241c20e69b
C7LF <ccal72@yahoo.co.uk>
2022-01-20 11:23:41 +0000

    Removed play command model from barrel index file

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,5 @@
 export * from "./command.model";
 export * from "./free-games.model";
-export * from "./play-command.model";
 export * from "./queue-construct.model";
 export * from "./request.model";
 export * from "./song.model";

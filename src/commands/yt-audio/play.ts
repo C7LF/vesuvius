1d0490748b97072101387fe5154d439864a93d2a
Cal <ccal72@yahoo.co.uk>
2022-01-20 11:14:27 +0000

    Feature/class refactor (#7)
    * Refactor commands into class
    * Removed supressEmbeds temporarily

--- src/commands/yt-audio/play.ts
+++ src/commands/yt-audio/play.ts
@@ -1,102 +1,100 @@
-import { prefix } from "../../config/command-config";
 import ytdl from "ytdl-core";
+import { Song, QueueContruct } from "../../models";
+import VesuviusCommand from "../../models/vesuvius-command.class";
 
-import { Song, QueueContruct, CommandPlay } from "../../models";
+const playExecute = () => async (msg: any, args?: string[]) => {
+  try {
+    const validYoutubeRegex =
+      /((http(s)?:\/\/)?)(www\.)?((youtube\.com\/)|(youtu.be\/))[\S]+/;
+    const isValidArgs = args && args[0] && args[0].match(validYoutubeRegex);
 
-const PlayCommand: CommandPlay = {
-  name: `${prefix}play`,
-  description: "Play audio from YouTube video",
-  async execute(msg: any, args) {
-    try {
-      const validYoutubeRegex = /((http(s)?:\/\/)?)(www\.)?((youtube\.com\/)|(youtu.be\/))[\S]+/;
-      const isValidArgs = args && args[0] && args[0].match(validYoutubeRegex);
+    if (!isValidArgs) {
+      return msg.channel.send("Invalid arguments!");
+    }
+    const queue = msg.client.queue;
+    const serverQueue = msg.client.queue.get(msg.guild?.id);
 
-      if (!isValidArgs) {
-        return msg.channel.send("Invalid arguments!");
-      }
-      const queue = msg.client.queue;
-      const serverQueue = msg.client.queue.get(msg.guild?.id);
+    const voiceChannel = msg.member?.voice.channel;
+    if (!voiceChannel)
+      return msg.channel.send(
+        "You need to be in a voice channel to play music!"
+      );
+    const permissions = voiceChannel.permissionsFor(msg.client.user);
+    if (!permissions?.has("CONNECT") || !permissions.has("SPEAK")) {
+      return msg.channel.send("I need the permissions to join and speak!");
+    }
 
-      const voiceChannel = msg.member?.voice.channel;
-      if (!voiceChannel)
-        return msg.channel.send(
-          "You need to be in a voice channel to play music!"
-        );
-      const permissions = voiceChannel.permissionsFor(msg.client.user);
-      if (!permissions?.has("CONNECT") || !permissions.has("SPEAK")) {
-        return msg.channel.send("I need the permissions to join and speak!");
-      }
+    if (!args) {
+      return msg.channel.send("Please enter a URL!");
+    }
 
-      if (!args) {
-        return msg.channel.send("Please enter a URL!");
-      }
+    const songInfo = await ytdl.getInfo(args[0]);
+    const song: Song = {
+      title: songInfo.videoDetails.title,
+      url: songInfo.videoDetails.video_url,
+    };
 
-      const songInfo = await ytdl.getInfo(args[0]);
-      const song: Song = {
-        title: songInfo.videoDetails.title,
-        url: songInfo.videoDetails.video_url,
+    if (!serverQueue) {
+      const queueContruct: QueueContruct = {
+        textChannel: msg.channel,
+        voiceChannel: voiceChannel,
+        connection: null,
+        songs: [],
+        volume: 5,
+        playing: true,
       };
 
-      if (!serverQueue) {
-        const queueContruct: QueueContruct = {
-          textChannel: msg.channel,
-          voiceChannel: voiceChannel,
-          connection: null,
-          songs: [],
-          volume: 5,
-          playing: true,
-        };
+      queue.set(msg.guild?.id, queueContruct);
 
-        queue.set(msg.guild?.id, queueContruct);
+      queueContruct.songs.push(song);
 
-        queueContruct.songs.push(song);
-
-        try {
-          let connection = await voiceChannel.join();
-          queueContruct.connection = connection;
-          this.play(msg, queueContruct.songs[0]);
-        } catch (err) {
-          console.log(err);
-          queue.delete(msg.guild?.id);
-          return msg.channel.send(err);
-        }
-      } else {
-        serverQueue.songs.push(song);
-        return msg.channel.send(
-          `**${song.title}** has been added to the queue!`
-        );
+      try {
+        let connection = await voiceChannel.join();
+        queueContruct.connection = connection;
+        play(msg, queueContruct.songs[0]);
+      } catch (err) {
+        console.log(err);
+        queue.delete(msg.guild?.id);
+        return msg.channel.send(err);
       }
-    } catch (error: any) {
-      console.log(error);
-      msg.channel.send(error.msg);
+    } else {
+      serverQueue.songs.push(song);
+      return msg.channel.send(`**${song.title}** has been added to the queue!`);
     }
-  },
-
-  play(msg: any, song: Song) {
-    const queue = msg.client.queue;
-    const guild = msg.guild;
-    const serverQueue = queue.get(msg.guild?.id);
-
-    if (!song) {
-      serverQueue.voiceChannel.leave();
-      queue.delete(guild?.id);
-      return;
-    }
-
-    const dispatcher = serverQueue.connection
-      .play(ytdl(song.url, { quality: "highestaudio", highWaterMark: 1 << 25 }))
-      .on("finish", () => {
-        serverQueue.songs.shift();
-        this.play(msg, serverQueue.songs[0]);
-      })
-      // TODO?: potentially repeat play steps if video metadata error occurs, try 5 times?
-      .on("error", (error: any) => {
-        msg.channel.send("Error playing");
-        console.error(error);
-      });
-    dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-    serverQueue.textChannel.send(`Started playing: **${song.title}**`);
-  },
+  } catch (error: any) {
+    console.log(error);
+    msg.channel.send(error.msg);
+  }
 };
 
-export = PlayCommand;
+const play = (msg: any, song: Song) => {
+  const queue = msg.client.queue;
+  const guild = msg.guild;
+  const serverQueue = queue.get(msg.guild?.id);
+
+  if (!song) {
+    serverQueue.voiceChannel.leave();
+    queue.delete(guild?.id);
+    return;
+  }
+
+  const dispatcher = serverQueue.connection
+    .play(ytdl(song.url, { quality: "highestaudio", highWaterMark: 1 << 25 }))
+    .on("finish", () => {
+      serverQueue.songs.shift();
+      play(msg, serverQueue.songs[0]);
+    })
+    // TODO?: potentially repeat play steps if video metadata error occurs, try 5 times?
+    .on("error", (error: any) => {
+      msg.channel.send("Error playing");
+      console.error(error);
+    });
+  dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+  serverQueue.textChannel.send(`Started playing: **${song.title}**`);
+};
+
+export = new VesuviusCommand(
+  "play",
+  "Play audio from YouTube video",
+  playExecute()
+);
--- src/commands/yt-audio/skip.ts
+++ src/commands/yt-audio/skip.ts
@@ -1,19 +1,17 @@
-import { prefix } from "../../config/command-config";
-import { Command } from "../../models";
+import VesuviusCommand from "../../models/vesuvius-command.class";
 
-const SkipCommand: Command = {
-  name: `${prefix}skip`,
-  description: "Skip to next track in queue",
-  execute(msg: any): void {
-    const serverQueue = msg.client.queue.get(msg.guild.id);
+const skipExecute = () => (msg: any) => {
+  const serverQueue = msg.client.queue.get(msg.guild.id);
 
-    if (!msg.member.voice.channel)
-      return msg.channel.send("You need to be in a voice channel to do that");
-    if (!serverQueue)
-      return msg.channel.send("There are no songs in the queue!");
+  if (!msg.member.voice.channel)
+    return msg.channel.send("You need to be in a voice channel to do that");
+  if (!serverQueue) return msg.channel.send("There are no songs in the queue!");
 
-    serverQueue.connection.dispatcher.end();
-  },
+  serverQueue.connection.dispatcher.end();
 };
 
-export = SkipCommand;
+export = new VesuviusCommand(
+  "skip",
+  "Skip to next track in queue",
+  skipExecute()
+);
